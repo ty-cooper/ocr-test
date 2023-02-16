5cc7863a71eeb1e21388933200a3f7d8cdb7b500
Tyler Cooper <91342671+ty-cooper@users.noreply.github.com>
2023-02-15 20:22:20 -0600

    feat: add OCR library

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from PIL import Image
+import pytesseract
 
 app = Flask(__name__)
 
@@ -6,3 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    if 'image' in request.files:
+        output_text = pytesseract.image_to_string(Image.open(request.files['image']))
+        return render_template("index.html", text=output_text)
+    else:
+        return render_template("index.html", text='')
