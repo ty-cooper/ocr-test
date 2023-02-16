afec8f21edbf55406058155c8f51e1bedd04cfb1
Tyler Cooper <91342671+ty-cooper@users.noreply.github.com>
2023-02-15 20:39:00 -0600

    feat: add test config to improve tesseract accuracy

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ def index():
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
     if 'image' in request.files:
-        output_text = pytesseract.image_to_string(Image.open(request.files['image']))
+        image = request.files['image']
+        output_text = pytesseract.image_to_string(Image.open(image), config='--psm 6 --oem 3 -l eng')
         return render_template("index.html", text=output_text)
     else:
         return render_template("index.html", text='')
